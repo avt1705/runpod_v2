5de46dcdf5129f294a52c5568a582838501e638b
Avtarsingh <109199789+avt1705@users.noreply.github.com>
2026-02-09 18:03:36 +0530

    Update handler.py
    .

--- handler.py
+++ handler.py
@@ -6,10 +6,9 @@ from io import BytesIO
 from diffusers import FluxPipeline
 from huggingface_hub import login
 
-# Authenticate with Hugging Face using the env var
+
 login(os.getenv("HUGGINGFACE_HUB_TOKEN"))
 
-# Load the model in bfloat16 (Required for FLUX stability and GQA support)
 pipe = FluxPipeline.from_pretrained(
     "black-forest-labs/FLUX.1-dev",
     torch_dtype=torch.bfloat16,
@@ -17,13 +16,11 @@ pipe = FluxPipeline.from_pretrained(
 ).to("cuda")
 
 def handler(event):
-    # Get user input prompt
     prompt = event.get("input", {}).get("prompt", None)
 
     if not prompt:
         return {"error": "No prompt provided in request."}
 
-    # Generate image
     image = pipe(
         prompt,
         height=1024,
@@ -32,7 +29,6 @@ def handler(event):
         num_inference_steps=50
     ).images[0]
 
-    # Save and encode
     buffer = BytesIO()
     image.save(buffer, format="PNG")
     image_b64 = base64.b64encode(buffer.getvalue()).decode("utf-8")
@@ -41,5 +37,5 @@ def handler(event):
         "image_base64": image_b64
     }
 
-#testing for new redeploy
 runpod.serverless.start({"handler": handler})
+
